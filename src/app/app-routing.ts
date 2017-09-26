7e66c7b4175bc8f961c466ce8a7ef67c22b08cd8
Venkata Ranga Kareti <venkataranga.kareti@OAKIT.COM>
2017-09-26 17:32:10 +0530

    route change

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { GlobalSearchComponent } from 'app/global-search/global-search.component';
-import {LocationMaintenanceComponent} from 'app/location-maintenance/location-maintenance.component';
-import {PromosCodesItemMaintenanceComponent} from 'app/promos-codes-item-maintenance/promos-codes-item-maintenance.component';
+import { LocationMaintenanceComponent } from 'app/location-maintenance/location-maintenance.component';
+import { PromosCodesItemMaintenanceComponent } from 'app/promos-codes-item-maintenance/promos-codes-item-maintenance.component';
 
 const routes: Routes = [
   {path: 'globalSearch', component: GlobalSearchComponent},
